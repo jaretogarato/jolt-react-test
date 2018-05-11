561fa152df4f326ce4af03cdaec82b609a432393
Jared Gallardo <jaredgg@gmail.com>
2018-05-11 11:54:02 -0600

    implement api call, add displayPeople function, add semantic-ui-react library

--- notes.md
+++ notes.md
@@ -1,3 +1,4 @@
 Start Time:  				End Time:
+5/11 11:03 AM
 
 Put any notes for us here.
--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
 import './Card.css';
+import { Segment, Header, Grid} from 'semantic-ui-react';
+
 
 class Card extends Component {
+  state = { people: [] }
+
+  componentDidMount() {
+    fetch('http://localhost:3008/people')
+      .then(data => data.json())
+      .then((data) => { this.setState({ people: data }) });
+  }
+
+  displayPeople = () => {
+    return this.state.people.map( person => {
+      return(
+        <Grid.Column width={4}>
+          <Segment>
+            <Header as='h1'>
+              Name: {person.name}
+              <br />
+              Image: {person.image}
+              <br />
+              Birthday: {person.birthyear}
+              <br />
+              Home Planet ID: {person.homeworld}
+            </Header>
+          </Segment>
+        </Grid.Column>
+      )
+    })
+  }
+
   render() {
-    return (
-      <div className='card'>
-        <div className='card-content'>
-          	<div className='card-name'>Darth Vader</div>
+    return(
+      <div>
+        <div className='card'>
+          <div className='card-content'>
+            <div className='card-name'>Darth Vader
+            </div>
           	<img src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
             <p>
-                <span>Birthday:</span>
-                <span>41.9BBY</span>
+              <span>Birthday:</span>
+              <span>41.9BBY</span>
             </p>
             <p>
-                {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
-                <span>Homeworld:</span>
-                <span>Tatooine</span>
+              {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
+              <span>Homeworld:</span>
+              <span>Tatooine</span>
             </p>
+          </div>
         </div>
-    </div>
 
+        <Segment basic>
+          <Grid>
+            <Grid.Row>
+              { this.displayPeople() }
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </div>
     );
   }
 }
